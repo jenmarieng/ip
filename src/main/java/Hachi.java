5521aca0e225f22f6814d212f3e2feb34c28d30e
jenmarieng <jermaineng04@gmail.com>
2025-01-23 19:41:05 +0800

    Add level-0

---
+++ src/main/java/Hachi.java
@@ -0,0 +1,5 @@
+public class Hachi {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Hachi\nWhat can I do for you?\n \nBye. Hope to see you again soon!");
+    }
+}
