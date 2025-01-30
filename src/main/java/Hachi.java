7cc52f624680e305cf9876132853ec091927fac6
jenmarieng <jermaineng04@gmail.com>
2025-01-30 13:21:26 +0800

    Add code to echo user commands and exit at bye

--- src/main/java/Hachi.java
+++ src/main/java/Hachi.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Hachi {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Hachi\nWhat can I do for you?\n \nBye. Hope to see you again soon!");
+        System.out.println("""
+                Hello! I'm Hachi
+                          __
+                 \\ ______/ U`-,    (WOOF!)
+                  (        /~~
+                 /_)^ --,/'
+                |_      |_
+                What can I do for you?""");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        do {
+            line = in.nextLine();
+            System.out.println(line);
+        } while (!line.equals("bye"));
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
